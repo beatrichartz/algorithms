fef6ddb51418564dc594dc091501ebec4e5fb154
Beat Richartz <beat.richartz@gmail.com>
2016-12-10 16:08:21 +0000

    Move description of example to connected time finder

--- src/beatrichartz/algorithms/quick_union/examples/social_network/FullyConnectedTimeFinder.java
+++ src/beatrichartz/algorithms/quick_union/examples/social_network/FullyConnectedTimeFinder.java
@@ -4,6 +4,13 @@ import org.joda.time.LocalDateTime;
 
 import java.util.Iterator;
 
+/**
+ Given a social network containing n members and a log file containing m timestamps at which times pairs of members
+ formed friendships, design an algorithm to determine the earliest time at which all members are connected
+ i.e., every member is a friend of a friend of a friend ... of a friend). Assume that the log file is sorted by
+ timestamp and that friendship is an equivalence relation. The running time of your algorithm should be m log⁡ n
+ or better and use extra space proportional to n.
+ */
 public class FullyConnectedTimeFinder {
     public static LocalDateTime findFromLog(Log log) {
         SocialNetwork socialNetwork = new SocialNetwork(log.maxNode() + 1);
--- src/beatrichartz/algorithms/quick_union/examples/social_network/SocialNetwork.java
+++ src/beatrichartz/algorithms/quick_union/examples/social_network/SocialNetwork.java
@@ -2,13 +2,6 @@ package beatrichartz.algorithms.quick_union.examples.social_network;
 
 import beatrichartz.algorithms.quick_union.WeightedQuickUnion;
 
-/**
- Given a social network containing n members and a log file containing m timestamps at which times pairs of members
- formed friendships, design an algorithm to determine the earliest time at which all members are connected
- i.e., every member is a friend of a friend of a friend ... of a friend). Assume that the log file is sorted by
- timestamp and that friendship is an equivalence relation. The running time of your algorithm should be m log⁡ n
- or better and use extra space proportional to n.
- */
 public class SocialNetwork extends WeightedQuickUnion {
     public SocialNetwork(int numNodes) {
         super(numNodes);
